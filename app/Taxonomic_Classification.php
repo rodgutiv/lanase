3572ae590655f31b16a4f6450d7a91eaaf6abe44
Martin <martinalanis.dev@gmail.com>
2017-04-18 12:57:12 -0500

    Agregado id a array del modelo de Taxonomic

--- app/Taxonomic_Classification.php
+++ app/Taxonomic_Classification.php
@@ -9,7 +9,7 @@ class Taxonomic_Classification extends Model
     //
     protected $table = "taxonomic_classifications";
 
-    protected $fillable = ['scientific_name', 'canonic_name','infra_generic','infra_epiteto_specific','rank_marker','modified','specific_epithet','superkingdom','kingdom','phylum','subphylum','superclass','class','subclass','infraclass','superorder','order_2','suborder','infraorder','parvorder','superfamily','family','subfamily','tribe','genus','subgenus','subspecie'];
+    protected $fillable = ['id','scientific_name', 'canonic_name','infra_generic','infra_epiteto_specific','rank_marker','modified','specific_epithet','superkingdom','kingdom','phylum','subphylum','superclass','class','subclass','infraclass','superorder','order_2','suborder','infraorder','parvorder','superfamily','family','subfamily','tribe','genus','subgenus','subspecie'];
 
     /**
      * El metodo es citations porque une con esa table pero la table pivote se llama
