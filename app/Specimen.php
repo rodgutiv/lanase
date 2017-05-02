e8b71baf8c3e7f244e212964dd38b23005bf08a9
AlfonsoCalderon <alfonsocc.isc@gmail.com>
2017-05-02 17:07:07 -0500

    Tabla specimens actualizada.

--- app/Specimen.php
+++ app/Specimen.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Specimen extends Model
 {
     //
-    protected $table = "specimens"
+    protected $table = "specimens";
 
     protected $fillable = ['taxonomy_id','media_id','literature_id','species_id','metadata_id','identifier_id','collection_id','sequence_id','user_id','family','genus'];
 
