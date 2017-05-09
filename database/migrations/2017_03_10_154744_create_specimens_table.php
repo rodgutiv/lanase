e7bcdc683535c7b444470e1910b40b3380f5fed5
AlfonsoCalderon <alfonsocc.isc@gmail.com>
2017-05-09 16:38:18 -0500

    Actualización de campos
    Tablas afectadas: "specimens" y "vernacular_name".

--- database/migrations/2017_03_10_154744_create_specimens_table.php
+++ database/migrations/2017_03_10_154744_create_specimens_table.php
@@ -19,19 +19,16 @@ class CreateSpecimensTable extends Migration
             $table->integer('taxonomy_id')->unsigned();
             $table->integer('media_id')->unsigned();
             $table->integer('literature_id')->unsigned();
-            $table->integer('specie_id')->unsigned();
             $table->integer('metadata_id')->unsigned();
             $table->integer('identifier_id')->unsigned();
             $table->integer('collection_id')->unsigned();
             $table->integer('user_id')->unsigned();
             $table->string('family', 45);
             $table->string('genus', 45);
-            $table->date('date')->nullable();
             $table->foreign('dataset_id')->references('id')->on('datasets')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('taxonomy_id')->references('id')->on('taxonomic_classifications')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('media_id')->references('id')->on('medias')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('literature_id')->references('id')->on('literatures')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreign('specie_id')->references('id')->on('species')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('metadata_id')->references('id')->on('metadata')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('identifier_id')->references('id')->on('identifiers')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('collection_id')->references('id')->on('collections')->onDelete('cascade')->onUpdate('cascade');
@@ -52,7 +49,6 @@ class CreateSpecimensTable extends Migration
             $table->dropForeign('specimens_taxonomy_id_foreign');
             $table->dropForeign('specimens_media_id_foreign');
             $table->dropForeign('specimens_literature_id_foreign');
-            $table->dropForeign('specimens_species_id_foreign');
             $table->dropForeign('specimens_metadata_id_foreign');
             $table->dropForeign('specimens_identifier_id_foreign');
             $table->dropForeign('specimens_collection_id_foreign');
